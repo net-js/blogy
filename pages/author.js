e93cf8d2fe08e7bb02389027a08a41bcf63fe37b
steve <103830370+net-js@users.noreply.github.com>
2022-06-04 14:02:34 +0530

    error 3

--- pages/author.js
+++ pages/author.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import css from "../styles/Author.module.css"
+import Link from "next/link"
 
 export default function Author(){
     return (
@@ -43,30 +44,30 @@ export default function Author(){
                 </script>
             </Head>
             <div itemScope itemType="http://schema.org/Person">
-                <a itemProp="url" href="https://github.com/net-js ">
+                <Link itemProp="url" href="https://github.com/net-js ">
                 <div itemProp="name"><h1>Net-js</h1></div>
-                </a>
+                </Link>
             </div>
             <div itemScope itemType="http://schema.org/Organization">
-                <a itemProp="url" href="https://github.com/net-js/blogy">
+                <Link itemProp="url" href="https://github.com/net-js/blogy">
                     <div itemProp="name"><h5>Blog-y</h5></div>
-                </a>
+                </Link>
             </div>
             <hr />
             <div style={{display: "none"}}>Job: <span  itemProp="jobtitle">Programer</span></div>
             <div itemProp="description">
                 <h2 id="hi-there-">Hi there 👋</h2>
-                <p>I net-js a self taught beginner doing both backend (mostly) and frontend. I use <a href="https://github.com/nodejs/node">Node.js</a></p>
+                <p>I net-js a self taught beginner doing both backend (mostly) and frontend. I use <Link href="https://github.com/nodejs/node">Node.js</Link></p>
                 <p>
                     I’m looking to collaborate on <strong>Node.js, Typescript, Front-end apps, Electron, JavaScript librarys</strong> etc;
                     <br />
                     <br />
-                    On StackOverflow: <a href="https://stackoverflow.com/users/18844713/steve?tab=profile"><strong>Steve</strong></a>;
+                    On StackOverflow: <Link href="https://stackoverflow.com/users/18844713/steve?tab=profile"><strong>Steve</strong></Link>;
                     <br />
-                    On NPMjs: <a href="https://www.npmjs.com/~net-js">net-js</a>
+                    On NPMjs: <Link href="https://www.npmjs.com/~net-js">net-js</Link>
                     <br />
-                    On Github: <a href="https://github.com/net-js">net-js</a></p>
-                <p>| sign --&gt; <a href="/sign.txt">sign.txt</a> <em>(<a href="https://gist.github.com/net-js/732043a7abf3bc2458425a63edd4df78">PublicKey</a> format is hex and rsa-sha265)</em> |</p>
+                    On Github: <Link href="https://github.com/net-js">net-js</Link></p>
+                <p>| sign --&gt; <Link href="/sign.txt"><a>sign.txt</a></Link> <em>(<Link href="https://gist.github.com/net-js/732043a7abf3bc2458425a63edd4df78">PublicKey</Link> format is hex and rsa-sha265)</em> |</p>
             </div>
         </div>
     )
